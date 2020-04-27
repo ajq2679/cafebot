50fc9981557866c422df472a147dc07eb4ac21aa
Andrew Quan <ajq2679@rit.edu>
2020-04-26 23:51:32 -0400

    added bad command, comments

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ const config = require("dotenv");
 const client = new Discord.Client({
   disableEveryone: true,
 });
+// dotenv directory for retrieving token 
 config.config({
   path: __dirname + "/.env",
 });
+// ready statement, logs to console that bot is online
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-
+// ping command
 client.on("message", (msg) => {
   if (msg.content === "ping") {
     msg.reply("Pong!");
   }
 });
-
+// detects when someone types "bad" and reacts with B A D
+client.on("message", (msg) => {
+    if (msg.content === "bad") {
+      msg.react('🇧').then(msg.react('🇦')).then(msg.react('🇩'));
+    }
+  });
+// bot login
 client.login(process.env.TOKEN);
